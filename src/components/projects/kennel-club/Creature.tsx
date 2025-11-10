2da569b12770b1749253f2740dc24d90372c916f
a1ts-a1t <unrelatedalt@gmail.com>
2025-11-09 21:21:41 -0800

    feat: websockets

--- src/components/projects/kennel-club/Creature.tsx
+++ src/components/projects/kennel-club/Creature.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { CreatureJson, Rect } from './types';
 import { Link } from 'react-router-dom';
 
@@ -6,14 +6,16 @@ interface CreatureProps {
     creature: CreatureJson;
     kennelWidth: number;
     kennelHeight: number;
+    onError: () => void;
 }
 
-export const Creature: React.FC<CreatureProps> = ({ creature, kennelWidth, kennelHeight }) => {
+export const Creature: React.FC<CreatureProps> = ({ creature, kennelWidth, kennelHeight, onError }) => {
     const [ rect, setRect ] = useState<Rect>({ left: 0, top: 0, width: 0, height: 0 });
+    const imageRef = useRef<HTMLImageElement>(null);
 
     useEffect(() => {
-        const width = creature.radius * kennelWidth;
-        const height = creature.radius * kennelHeight;
+        const width = 2 * creature.radius * kennelWidth;
+        const height = 2 * creature.radius * kennelHeight;
 
         const left = creature.position.x * kennelWidth - width;
         const top = creature.position.y * kennelHeight - height;
@@ -29,6 +31,25 @@ export const Creature: React.FC<CreatureProps> = ({ creature, kennelWidth, kenne
         });
     }, [ creature, kennelWidth, kennelHeight ]);
 
+    // refresh image
+    useEffect(() => {
+        if (imageRef.current === null) {
+            return;
+        }
+
+        // this is kind of hacky but whatever lmao
+        // https://stackoverflow.com/questions/1077041/refresh-image-with-a-new-one-at-the-same-url/
+        imageRef.current.src = `https://alts-alt.online${creature.sprite_path}#${new Date().getTime()}`;
+    }, [ creature ]);
+
+    useEffect(() => {
+        if (imageRef.current === null) {
+            return;
+        }
+
+        imageRef.current.addEventListener('error', onError);
+    }, []);
+
     return (
         <Link
             key={ creature.id }
@@ -40,7 +61,7 @@ export const Creature: React.FC<CreatureProps> = ({ creature, kennelWidth, kenne
             title={ `Link to ${creature.id} website` }
             style={ { ...rect } }
         >
-            <img src={ `https://alts-alt.online${creature.sprite_path}` } alt={ `${creature.id} sprite` } />
+            <img className="size-full object-cover" ref={ imageRef } alt={ `${creature.id} sprite` } />
         </Link>
     );
 };
