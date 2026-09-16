b569ceb1bb855ccb7367131ac08bbceb199e158d
a1ts-a1t <unrelatedalt@gmail.com>
2026-09-16 14:21:56 -0700

    chore: replace site with astro migration

---
+++ astro.config.mjs
@@ -0,0 +1,50 @@
+// @ts-check
+import node from "@astrojs/node";
+import { defineConfig, envField, fontProviders } from "astro/config";
+
+const isVpsTarget = process.env.PUBLIC_TARGET === "vps";
+
+export default defineConfig({
+  ...(isVpsTarget ? { adapter: node({ mode: "standalone" }) } : {}),
+  site: isVpsTarget ? "https://alts-alt.online" : "https://alts-alt.neocities.org",
+  env: {
+    schema: {
+      PUBLIC_TARGET: envField.enum({
+        context: "client",
+        access: "public",
+        values: ["neocities", "vps"],
+        default: "vps",
+      }),
+    },
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Ubuntu Mono",
+      cssVariable: "--font-ubuntu-mono",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/UbuntuMono-Regular.ttf"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+  vite: {
+    resolve: {
+      extensions: [
+        ".astro",
+        ".mjs",
+        ".js",
+        ".mts",
+        ".ts",
+        ".jsx",
+        ".tsx",
+        ".json",
+      ],
+    },
+  },
+});
